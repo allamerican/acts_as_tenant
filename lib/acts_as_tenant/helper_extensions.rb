4d1b0971baa4a1687c2c95875ca9f5317153cba2
Vishal Jain <vishal@kiprosh.com>
2022-02-11 11:13:21 +0530

    Add support for nested without_tenant block

--- lib/acts_as_tenant/helper_extensions.rb
+++ lib/acts_as_tenant/helper_extensions.rb
@@ -5,14 +5,21 @@ module ActsAsTenant
         raise ArgumentError, "block required"
       end
 
-      ActsAsTenant.original_tenant = ActsAsTenant.current_tenant
-      ActsAsTenant.current_tenant = nil
-      value = block.call
-      return value
+      # if original_tenant is nil, assume we are not already in a without_tenant block
+      # possible value is nil AND we're in a block if ActsAsTenant.current_tenant was nil, but that shouldn't matter since everything stays nil
+      if ActsAsTenant.original_tenant.nil?
+        ActsAsTenant.original_tenant = ActsAsTenant.current_tenant
+        ActsAsTenant.current_tenant = nil
+      else
+        within_block = true
+      end
 
+      block.call
     ensure
-      ActsAsTenant.current_tenant = ActsAsTenant.original_tenant
-      ActsAsTenant.original_tenant = nil
+      unless within_block
+        ActsAsTenant.current_tenant = ActsAsTenant.original_tenant
+        ActsAsTenant.original_tenant = nil
+      end
     end
   end
 end
